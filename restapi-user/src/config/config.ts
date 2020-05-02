3964f1788a0e6393877e42a38ac49c1e01a297a2
alew <ale.w.es@sync.money>
2020-05-02 21:06:05 +0200

    initial commit

---
+++ restapi-user/src/config/config.ts
@@ -0,0 +1,20 @@
+export const config = {
+  "dev": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET,
+    "jwt_secret": process.env.JWT_SECRET
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
